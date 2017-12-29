244ba9da46b247c7bda2047379257f1dd0772c94
PumpkinDeveloper <32527786+PumpkinDeveloper@users.noreply.github.com>
2017-12-29 18:08:53 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,27 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const prefix = ";";
+
+const args = message.content.slice(prefix.length).trim().split(/ +/g);
+const command = args.shift().toLowerCase();
+var requestify = require('requestify'); 
 
 client.on('ready', () => {
     console.log('Ready.');
 });
 
 client.on('message', message => {
-    if (message.guild.name === "Asta's Accounts") {
-    	console.log(message.content);
-  	}
+      if(command === 'buy') {
+          let member = message.mentions.members.first();
+          let clientrole = message.guild.roles.find("name", "Client");
+          
+          const embed = new Discord.RichEmbed()
+          .setTitle("Buy access")
+          .setAuthor(client.user.username, client.user.avatarURL)
+          .setColor(0x00AE86)
+          .setDescription("You can buy access at !url!")
+          
+      }
 });
 
 // THIS  MUST  BE  THIS  WAY
